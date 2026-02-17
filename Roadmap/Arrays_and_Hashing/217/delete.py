54584cd1720eb308408792b598b8eda7c7b5a39e
Bill Moorby <billmoorby@gmail.com>
2026-02-17 11:42:27 -0800

    contains duplicate

---
+++ Roadmap/Arrays_and_Hashing/217/delete.py
@@ -0,0 +1,9 @@
+class Solution:
+  def containsDuplicate(self, nums):
+    dict_map = {}
+
+    for n in nums:
+      if n in dict_map:
+        return True
+      dict_map[n] = dict_map.get(n, 0)+1
+    return False
---
+++ Roadmap/Arrays_and_Hashing/217/delete_test.py
@@ -0,0 +1,13 @@
+import unittest
+from delete import Solution
+
+class TestSol(unittest.TestCase):
+  def test_sol(self):
+    sol = Solution()
+
+    self.assertEqual(sol.containsDuplicate([1, 2, 3, 1]), True, 'Wrong output!')
+    self.assertEqual(sol.containsDuplicate([1, 2, 3, 4]), False, 'Wrong output!')
+    self.assertEqual(sol.containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]), True, 'Wrong output!')
+
+if __name__ == '__main__':
+  unittest.main()
