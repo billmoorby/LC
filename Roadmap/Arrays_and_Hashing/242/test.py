c0ba660081fe2d1cfc701174e8ba7c5ac890b5b7
Bill Moorby <billmoorby@gmail.com>
2026-02-17 12:27:46 -0800

    valid anagram

---
+++ Roadmap/Arrays_and_Hashing/242/test.py
@@ -0,0 +1,12 @@
+import unittest
+from sol import Solution
+
+class TestSol(unittest.TestCase):
+  def test_sol(self):
+    sol = Solution()
+
+    self.assertEqual(sol.isAnagram('anagram', 'nagaram'), True, 'Wrong output!')
+    self.assertEqual(sol.isAnagram('rat', 'car'), False, 'Wrong output!')
+
+if __name__ == '__main__':
+  unittest.main()
