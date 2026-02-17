538761873c2364b80c346c4e3c29b74b7f1996a6
Bill Moorby <billmoorby@gmail.com>
2026-02-16 23:13:34 -0800

    Two Sum

---
+++ Google_LC/Easy/1/sol.py
@@ -0,0 +1,10 @@
+class Solution:
+  def twoSum(self, nums, target):
+    dict_map = {}
+
+    for index, num in enumerate(nums):
+      diff = target - num
+      if diff in dict_map:
+        return [dict_map[diff], index]
+      dict_map[num] = index
+      
