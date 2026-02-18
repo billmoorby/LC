4bcef32cef66c48b45c3f6f069f9975b62032b8f
Bill Moorby <billmoorby@gmail.com>
2026-02-18 11:10:51 -0800

    top k frequent elements

---
+++ Roadmap/Arrays_and_Hashing/347/delete_test.py
@@ -0,0 +1,24 @@
+import unittest
+from delete import Solution
+
+class TestSol(unittest.TestCase):
+
+  def test_sol(self):
+    sol = Solution()
+
+    valid_outputs_1 = [
+      [1, 2], 
+      [2, 1]
+    ]
+
+    valid_outputs_3 = [
+      [1, 2], 
+      [2, 1]
+    ]
+
+    self.assertIn(sol.topKFrequent([1, 1, 1, 2, 2, 3], 2), valid_outputs_1, 'Wrong output!')
+    self.assertEqual(sol.topKFrequent([1], 1), [1], 'Wrong output!')
+    self.assertIn(sol.topKFrequent([1, 2, 1, 2, 1, 2, 3, 1, 3, 2], 2), valid_outputs_3, 'Wrong output!')
+
+if __name__ == '__main__':
+  unittest.main() 
---
+++ Roadmap/Arrays_and_Hashing/347/sol.py
@@ -0,0 +1,19 @@
+class Solution:
+  
+  def topKFrequent(self, nums, k):
+    freq_map = {}
+    buckets = [[] for i in range(len(nums)+1)]
+    res = []
+
+    for n in nums:
+      freq_map[n] = freq_map.get(n, 0)+1
+
+    for n, freq in freq_map.items():
+      buckets[freq].append(n)
+    
+    for i in range(len(buckets)-1, -1, -1):
+      if buckets[i]:
+        for j in buckets[i]:
+          res.append(j)
+          if len(res) == k:
+            return res
