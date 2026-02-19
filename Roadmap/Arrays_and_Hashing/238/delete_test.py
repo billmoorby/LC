6ea18369f2966987297ace09016846293ad9bbdd
Bill Moorby <billmoorby@gmail.com>
2026-02-18 16:21:34 -0800

    product of array except self

---
+++ Roadmap/Arrays_and_Hashing/238/delete_test.py
@@ -0,0 +1,12 @@
+import unittest
+from delete import Solution
+
+class TestSol(unittest.TestCase):
+  def test_sol(self):
+    sol = Solution()
+
+    self.assertEqual(sol.productExceptSelf([1, 2, 3, 4]), [24, 12, 8, 6], 'Wrong output!')
+    self.assertEqual(sol.productExceptSelf([-1, 1, 0, -3, 3]), [0, 0, 9, 0, 0], 'Wrong output!')
+  
+if __name__ == '__main__':
+  unittest.main()
