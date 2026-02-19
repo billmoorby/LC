8079512f555a083ff68b665a21d4fa2796245539
Bill Moorby <billmoorby@gmail.com>
2026-02-18 23:47:14 -0800

    longest consecutive sequence

---
+++ Roadmap/Arrays_and_Hashing/128/delete.py
@@ -0,0 +1,21 @@
+class Solution:
+
+  def longestConsecutive(self, nums):
+    if not nums:
+      return 0
+    
+    num_set = set(nums)
+    max_streak = 0
+
+    for n in num_set:
+      if (n-1) not in num_set:
+        current_streak = 1
+
+        while (n+1) in num_set:
+          n+=1
+          current_streak+=1
+        
+        max_streak = max(max_streak, current_streak)
+    
+    return max_streak
+  
---
+++ Roadmap/Arrays_and_Hashing/128/delete_test.py
@@ -0,0 +1,13 @@
+import unittest
+from delete import Solution
+
+class TestSol(unittest.TestCase):
+  def test_sol(self):
+    sol = Solution()
+
+    self.assertEqual(sol.longestConsecutive([100, 4, 200, 1, 3, 2]), 4, 'Wrong output!')
+    self.assertEqual(sol.longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]), 9, 'Wrong output!')
+    self.assertEqual(sol.longestConsecutive([1, 0, 1, 2]), 3, 'Wrong output!')
+
+if __name__ == '__main__':
+  unittest.main()
