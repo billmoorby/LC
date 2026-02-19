0003404ef7fd5cbae277a51b3519982ebb7b804a
Bill Moorby <billmoorby@gmail.com>
2026-02-18 23:14:38 -0800

    valid sudoku

---
+++ Roadmap/Arrays_and_Hashing/36/sol.py
@@ -0,0 +1,26 @@
+import collections
+
+class Solution:
+  def isValidSudoku(self, board):
+    N = 9
+    row_map = collections.defaultdict(set)
+    col_map = collections.defaultdict(set)
+    square_map = collections.defaultdict(set)
+
+    for r in range(N):
+      for c in range(N):
+        if board[r][c] == '.':
+          continue
+
+        if (board[r][c] in row_map[r] or 
+            board[r][c] in col_map[c] or 
+            board[r][c] in square_map[(r//3, c//3)]):
+          
+          return False
+        
+        else:
+          row_map[r].add(board[r][c])
+          col_map[c].add(board[r][c])
+          square_map[(r//3, c//3)].add(board[r][c])
+
+    return True
