a239fd6d7168dafc0f85ffbb6437493a5b12f2a5
Bill Moorby <billmoorby@gmail.com>
2026-03-27 13:46:16 -0700

    Container with most water

---
+++ Roadmap/Two_Pointers/11/test.py
@@ -0,0 +1,13 @@
+import unittest
+from sol import Solution
+
+class TestSol(unittest.TestCase):
+
+  def test_sol(self):
+    sol = Solution()
+
+    self.assertEqual(sol.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]), 49, 'Wrong output!')
+    self.assertEqual(sol.maxArea([1, 1]), 1, 'Wrong output!')
+
+if __name__ == '__main__':
+  unittest.main()
